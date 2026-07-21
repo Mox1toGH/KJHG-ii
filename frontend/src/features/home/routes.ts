de71c45f8d5cc8a7f3afb301fe2b02e49c7192d1
Lastkngiht12 <vovaua27@gmail.com>
2026-07-21 15:53:44 +0300

    sadasdasd

--- frontend/src/features/home/routes.ts
+++ frontend/src/features/home/routes.ts
@@ -2,10 +2,12 @@ export const homeRoutes = [
   {
     path: '/',
     component: () => import('@/features/home/pages/HomeDashboardPage.vue'),
+    meta: { requiresAuth: true },
   },
   {
     path: '/map',
     name: 'home-map',
     component: () => import('@/features/home/pages/HomeMapPage.vue'),
+    meta: { requiresAuth: true },
   },
 ]
